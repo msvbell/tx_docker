9091df7b5ac022765e32c5779100d15f0bda7567
msvbell <msvbell@gmail.com>
2019-12-30 15:22:21 +0300

    svn init

--- scripts/init_repository.sh
+++ scripts/init_repository.sh
@@ -1,15 +1,25 @@
 #!/bin/bash
 
+# init svn repository
 if [ ! -d "$REPOSITORY_DIR" ]; then
   /opt/csvn/bin/svnadmin create "$REPOSITORY_DIR"
-#  echo "$SVN_USER = $SVN_PASSWORD" >> "$REPOSITORY_DIR"/conf/passwd
   sed -i "s/# anon-access/anon-access/" "$REPOSITORY_DIR"/conf/svnserve.conf
   sed -i "s/# auth-access/auth-access/" "$REPOSITORY_DIR"/conf/svnserve.conf
   sed -i "s/# realm = My First Repository/realm = $SVN_USER/" "$REPOSITORY_DIR"/conf/svnserve.conf
   sed -i "s/# password-db = passwd/password-db = $SVN_PASSWORD/" "$REPOSITORY_DIR"/conf/svnserve.conf
-
 fi
 
-echo "Hello there!"
+sed -i "s/Name=\"TEST\"/Name=\"$TEST_DB_NAME\"/" /tmp/repository.dumpfile
+sed -i "s#jdbc:oracle:thin:@localhost:1521/TEST#jdbc:oracle:thin:@localhost:1521/$TEST_DB_NAME#" /tmp/repository.dumpfile
+sed -i "s/Schema=\"TEST\"/Schema=\"$TEST_DB_NAME\"/" /tmp/repository.dumpfile
+sed -i "s/Name=\"PROD\"/Name=\"$PROD_DB_NAME\"/" /tmp/repository.dumpfile
+sed -i "s#jdbc:oracle:thin:@localhost:1521/PROD#jdbc:oracle:thin:@localhost:1521/$PROD_DB_NAME#" /tmp/repository.dumpfile
+sed -i "s/Schema=\"PROD\"/Schema=\"$PROD_DB_NAME\"/" /tmp/repository.dumpfile
+
+# import tranzaxis files to svn
+# /opt/csvn/bin/svn import --username admin --password admin -m "Init" /tmp/repository_files/ http://localhost:18080/svn/TX
+/opt/csvn/bin/svnadmin load $REPOSITORY_DIR < /tmp/repository.dumpfile
+
+rm -rf /tmp/repository_files /tmp/repository.dumpfile
+
 /config/bootstrap.sh
-echo "Hello there too!"
